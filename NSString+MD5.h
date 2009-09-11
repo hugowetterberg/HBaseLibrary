0ff86643867ef7f612aa9b385b55c4449d8c491b
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2009-09-11 08:15:11 +0200

    Added string category that adds method for generating the MD5 for a string.

---
+++ NSString+MD5.h
@@ -0,0 +1,16 @@
+//
+//  NSString+MD5.h
+//  HBaseLibrary
+//
+//  Created by Hugo Wetterberg on 2009-09-08.
+//  Copyright 2009 Good Old. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (MD5)
+
+- (NSString *)MD5;
+
+@end
